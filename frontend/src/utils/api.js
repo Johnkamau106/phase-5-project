202b997c590acdaffe1cb985a474cb4ffb32c1a1
Your Name <johnkamau106@gmail.com>
2025-07-30 10:20:57 +0300

    last

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -83,7 +83,7 @@
 //     }
 //   };
 // };
-export const BASE_URL = "http://localhost:5000"; // Change if deployed
+export const BASE_URL = "https://phase-5-project-hbsk-9tb0hdeau-john-kamaus-projects-c0b9c885.vercel.app";
 
 export const loginUser = async ({ email, password }) => {
   try {
